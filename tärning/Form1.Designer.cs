8283b13e62c8026742079f2040e8a10c4a78a76b
christerljung <allcat@spray.se>
2019-02-06 19:32:04 +0100

    sdfg

--- tärning/Form1.Designer.cs
+++ tärning/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox6 = new System.Windows.Forms.PictureBox();
@@ -35,6 +36,7 @@
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
@@ -45,9 +47,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(233, 47);
+            this.button1.Location = new System.Drawing.Point(350, 72);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 42);
+            this.button1.Size = new System.Drawing.Size(231, 65);
             this.button1.TabIndex = 0;
             this.button1.Text = "kasta tärning";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,9 +59,10 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = global::tärning.Properties.Resources.sex;
-            this.pictureBox4.Location = new System.Drawing.Point(472, 242);
+            this.pictureBox4.Location = new System.Drawing.Point(708, 372);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox4.Size = new System.Drawing.Size(150, 97);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 7;
             this.pictureBox4.TabStop = false;
@@ -66,9 +70,10 @@
             // pictureBox6
             // 
             this.pictureBox6.Image = global::tärning.Properties.Resources.tre;
-            this.pictureBox6.Location = new System.Drawing.Point(472, 145);
+            this.pictureBox6.Location = new System.Drawing.Point(708, 223);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox6.Size = new System.Drawing.Size(150, 97);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 6;
             this.pictureBox6.TabStop = false;
@@ -76,9 +81,10 @@
             // pictureBox5
             // 
             this.pictureBox5.Image = global::tärning.Properties.Resources.två;
-            this.pictureBox5.Location = new System.Drawing.Point(261, 145);
+            this.pictureBox5.Location = new System.Drawing.Point(392, 223);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox5.Size = new System.Drawing.Size(150, 97);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 5;
             this.pictureBox5.TabStop = false;
@@ -86,9 +92,10 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::tärning.Properties.Resources.fyra;
-            this.pictureBox3.Location = new System.Drawing.Point(43, 242);
+            this.pictureBox3.Location = new System.Drawing.Point(64, 372);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox3.Size = new System.Drawing.Size(150, 97);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
@@ -96,9 +103,10 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::tärning.Properties.Resources.fem;
-            this.pictureBox2.Location = new System.Drawing.Point(261, 242);
+            this.pictureBox2.Location = new System.Drawing.Point(392, 372);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox2.Size = new System.Drawing.Size(150, 97);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
@@ -106,18 +114,24 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::tärning.Properties.Resources.ett;
-            this.pictureBox1.Location = new System.Drawing.Point(43, 145);
+            this.pictureBox1.Location = new System.Drawing.Point(64, 223);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 63);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 97);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(675, 413);
+            this.ClientSize = new System.Drawing.Size(1012, 635);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox6);
             this.Controls.Add(this.pictureBox5);
@@ -125,6 +139,7 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -147,6 +162,7 @@
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- tärning/Form1.cs
+++ tärning/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace tärning
 {
@@ -16,51 +17,78 @@ namespace tärning
             InitializeComponent();
         }
 
+        int i = 0;
+      
+            int[] talArray = new int[50]; //skapa en array med 50 platser
+        Random r ;      //Gör ett slumptal mellan 5 och 50  
+        int slump ;
+
+        Random p = new Random();      //Gör ett slumptal mellan 5 och 50  
+        int tärning = 0;
         private void Form1_Load(object sender, EventArgs e)
         {
-            pictureBox1.Enabled = false;
-            pictureBox2.Enabled = false;
-            pictureBox3.Enabled = false;
-            pictureBox4.Enabled = false;
-            pictureBox5.Enabled = false;
-            pictureBox6.Enabled = false;
+
+            
+           
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            i = 0;
+            r = new Random();      //Gör ett slumptal mellan 5 och 50
+            slump = r.Next(5, 50);
             kasta_tärning();
+            SetAllInvisible();
         }
         private void kasta_tärning()
-        {
-            int[] talArray = new int[50]; //skapa en array med 50 platser
-            Random r = new Random();      //Gör ett slumptal mellan 5 och 50  
-            int slump = r.Next(5, 50);
-
-            Random p = new Random();      //Gör ett slumptal mellan 5 och 50  
-            int tärning = 0;
+        {         
 
             for (int i = 0; i < slump; i++)
             {
                 tärning = p.Next(1, 6);
                 talArray[i] = tärning;
             }
-
+                      
            
-            for (int i = 0; i < slump; i++)
-            {
-                if (talArray[i] == 1)
-                    pictureBox1.Enabled = true;
-                if (talArray[i] == 1)
-                    pictureBox2.Enabled = true;
-                if (talArray[i] == 1)
-                    pictureBox3.Enabled = true;
-                if (talArray[i] == 1)
-                    pictureBox4.Enabled = true;
-                if (talArray[i] == 1)
-                    pictureBox5.Enabled = true;
-                if (talArray[i] == 1)
-                    pictureBox6.Enabled = true;
-            }
+            timer1.Enabled = true;
+
         }
+        private void SetAllInvisible()
+        {
+
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            pictureBox6.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            i++;            
+              //  SetAllInvisible();
+                if (talArray[i] == 1)
+                    pictureBox1.Visible = true;
+                if (talArray[i] == 2)
+                    pictureBox2.Visible = true;
+                if (talArray[i] == 3)
+                    pictureBox3.Visible = true;
+                if (talArray[i] == 4)
+                    pictureBox4.Visible = true;
+                if (talArray[i] == 5)
+                    pictureBox5.Visible = true;
+                if (talArray[i] == 6)
+                    pictureBox6.Visible = true;
+            if (i > slump)
+                timer1.Enabled = false;
+        }
+        
     }
 }
